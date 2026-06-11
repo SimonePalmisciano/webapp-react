5c1fce6e76303458985fb9b3e6d3c01f8e946d9f
federicoacker <acker.federico@gmail.com>
2026-06-11 16:13:34 +0200

    aggiunto grow alla parte in mezzo

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,15 +2,15 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 function NotFound() {
-  return (
-    <div className="d-flex vh-100 flex-column">
-        <Header/>
-        <div>
-            <h2><strong>404</strong> - Page Not Found</h2>
+    return (
+        <div className="d-flex vh-100 flex-column">
+            <Header />
+            <div className="flex-grow-1">
+                <h2><strong>404</strong> - Page Not Found</h2>
+            </div>
+            <Footer />
         </div>
-        <Footer/>
-    </div>
-  )
+    )
 }
 
 export default NotFound
