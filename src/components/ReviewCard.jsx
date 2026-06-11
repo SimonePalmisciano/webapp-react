284707ebde50fbf322b00a29827dd4fbf84bac70
federicoacker <acker.federico@gmail.com>
2026-06-11 17:37:31 +0200

    Aggiunta reviewCard

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,8 +1,20 @@
 
-function ReviewCard() {
-  return (
-    <div>ReviewCard</div>
-  )
+function ReviewCard({ review }) {
+    return (
+        <div className="card">
+            <div className="card-header d-flex justify-content-between">
+                <div className="card-title">
+                    {review.title}
+                </div>
+                <div className="btn btn-primary">
+                    <i className="bi bi-hand-thumbs-up"></i>
+                </div>
+            </div>
+            <div className="card-body">
+                {review.description}
+            </div>
+        </div>
+    )
 }
 
 export default ReviewCard
