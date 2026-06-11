e5109f0d6dfba2c12f9e346b94b1f81cb9a398ab
federicoacker <acker.federico@gmail.com>
2026-06-11 16:37:32 +0200

    Aggiunti 3 file che mancavano per ora vuoti

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { ProductProvider } from "./contexts/ProductContext.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import { ProductDetailPage } from "./pages/ProductDetailPage.jsx"
-
+import NotFound from "./pages/NotFound.jsx";
+import LayoutPagina from "./layouts/LayoutPagina.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
 
 
 function App() {
