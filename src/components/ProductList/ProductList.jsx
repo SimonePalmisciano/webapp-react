5badaa7813c7fbbcc42b43e270f90c113a7229e2
federicoacker <acker.federico@gmail.com>
2026-06-15 09:48:44 +0200

    Aggiunto il not found per la search

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,6 +6,7 @@ function ProductList({products}){
     return(
         <div className="container py-4">
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                {products.length === 0 && <div className="alert alert-warning w-100 text-center">🦕 I nostri cacciatori non hanno trovato nessun dinosauro che ti soddisfi...  🦕</div>}
                 {products.map((product) => (
                     <div className="col" key={product.slug}>
                         <ProductCard product={product}/>
