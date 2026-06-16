01fbc6ddf1454734eecd576c427e9778cf22dd7d
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-06-16 13:01:53 +0200

    creato context per salvare la visita dell'utente

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,30 @@ import AboutUs from "./pages/AboutUs.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
 import { CategoryProvider } from "./contexts/CategoryContext.jsx";
 import ScrollToTop from "./components/ScrollToTop.js";
+import { VisitProvider } from "./contexts/VisitContext.jsx";
 
 function App() {
 
   return (
 
-
+    <VisitProvider>
       <CategoryProvider>
-      <BrowserRouter>
-        <ScrollToTop/>
-        <Routes>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routes>
 
-          <Route Component={LayoutPagina}>
-            <Route index Component={HomePage} />
-            <Route path="products" Component={ProductPage} />
-            <Route path="products/:productSlug" Component={ProductDetailPage} />
-            <Route path="about-us" Component={AboutUs} />
-          </Route>
+            <Route Component={LayoutPagina}>
+              <Route index Component={HomePage} />
+              <Route path="products" Component={ProductPage} />
+              <Route path="products/:productSlug" Component={ProductDetailPage} />
+              <Route path="about-us" Component={AboutUs} />
+            </Route>
 
-          <Route path="*" Component={NotFound} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" Component={NotFound} />
+          </Routes>
+        </BrowserRouter>
       </CategoryProvider>
+    </VisitProvider>
 
 
   )
