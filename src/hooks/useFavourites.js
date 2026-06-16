aa771e6d63339222a10fcd418fe5ff58eaf56004
federicoacker <acker.federico@gmail.com>
2026-06-16 19:04:29 +0200

    Fixato 2 typo

--- src/hooks/useFavourites.js
+++ src/hooks/useFavourites.js
@@ -4,7 +4,7 @@ import { FavouritesContext } from "../contexts/FavouritesContext";
 function useFavourites(){
     const favouriteValues = useContext(FavouritesContext);
     if(!favouriteValues) {
-        throw new Error("Hai dimenticato di mettere il CategoryProvider");
+        throw new Error("Hai dimenticato di mettere il FavouritesProvider");
     }
 
     return favouriteValues;
